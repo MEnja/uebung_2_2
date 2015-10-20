9327373a952b813a6479f25ab69d1f0435b63cc0
14337 <14337@A001-09.nordakademie.intern>
2015-10-20 11:00:29 +0200

    Aufgabe erledigt

---
+++ src/BinomischeFormel.java
@@ -0,0 +1,21 @@
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a = 8;
+		double b = 3;
+		
+		double Erstens = a * a;
+		double Zweitens = 2 * a * b;
+		double Drittens = b * b;
+				
+		double loesung1 = Erstens + Zweitens + Drittens;
+		
+		
+		System.out.println(" (a+b)² = " + loesung1);
+		
+	}
+
+}
